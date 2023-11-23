7c3fac108998a92838b774ea5646f44f1c3276d2
Parthiv0311 <122387846+Parthiv0311@users.noreply.github.com>
2023-11-22 22:44:20 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Dynamically adding student information
+    document.getElementById('studentInfo').innerText = 'Student ID: 200543216, Name: Parthiv ';
+
+    // Event listener for form submission
+    document.getElementById('pizzaOrderForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        console.log("Form submitted");
+        // Capture form data
+        const formData = {
+            name: document.getElementById('name').value,
+            contact: document.getElementById('contact').value,
+            pizzaType: document.getElementById('pizza_type').value,
+            toppings: document.getElementById('toppings').value,
+            extraCheese: document.getElementById('extra_cheese').checked,
+            quantity: document.getElementById('quantity').value,
+            specialInstructions: document.getElementById('special_instructions').value
+        };
+
+        // Create Pizza object
+        const pizzaOrder = new Pizza(formData.pizzaType, formData.toppings, formData.extraCheese, formData.quantity, formData.specialInstructions);
+        
+        // Display pizza description
+        pizzaOrder.displayOrderDetails();
+    });
+});
+
+// Pizza class
+class Pizza {
+    constructor(type, toppings, extraCheese, quantity, instructions) {
+        this.type = type;
+        this.toppings = toppings;
+        this.extraCheese = extraCheese;
+        this.quantity = quantity;
+        this.instructions = instructions;
+    }
+
+    displayOrderDetails() {
+        const orderDetails = `Pizza Type: ${this.type}
+Toppings: ${this.toppings}
+Extra Cheese: ${this.extraCheese ? 'Yes' : 'No'}
+Quantity: ${this.quantity}
+Special Instructions: ${this.instructions}`;
+
+        alert(orderDetails);
+    }
+}
